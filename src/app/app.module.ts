6f636c554a2152a98fe2ec44a4aaefc127d1b0fa
Nick Prühs <dev@npruehs.de>
2020-03-07 21:50:52 +0100

    Add top bar and empty account component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    AccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: AccountComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
